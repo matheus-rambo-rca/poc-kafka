bd97aebe9e90fe85f691bc6c0687a1ce484732a5
rca_matheus_rambo <matheus.rambo@operacao.rcadigital.com.br>
2020-01-24 12:42:58 -0300

    init

---
+++ src/main/java/com/br/rca/pockafka/PocKafkaApplication.java
@@ -0,0 +1,14 @@
+package com.br.rca.pockafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocKafkaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Oii");
+		SpringApplication.run(PocKafkaApplication.class, args);
+	}
+
+}
