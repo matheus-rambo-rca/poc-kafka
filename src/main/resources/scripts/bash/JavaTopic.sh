7e8ccdcaaa620c0f29f4d51a67c4d407690d1fd3
matheus-rambo-rca <matheus.rambo@operacao.rcadigital.com.br>
2020-01-29 11:45:09 -0300

    utilizando o docker compose

--- src/main/resources/scripts/bash/JavaTopic.sh
+++
@@ -1,8 +0,0 @@
-# Onde KAFKA HOME é onde se encontra o diretório do KAFKA
-# 127.0.0.1 -> localhost
-
-KAFKA_HOME/bin/kafka-topics.sh --create \
-    --zookeeper 127.0.0.1:2181 \
-    --replication-factor 1 \
-    --partitions 1 \
-    --topic javatopic
